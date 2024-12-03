fc4ef4715c534afd69187959acd16b49f41a8e5c
Pablo Gómez <pgr866@inlumine.ual.es>
2024-12-03 16:37:44 +0100

    Commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "@/components/mode-toggle"
+
+import { Button } from "@/components/ui/button"
+
 import { DateRangePicker } from 'rsuite'
 import 'rsuite/dist/rsuite.css'
 document.head.insertAdjacentHTML('beforeend', `<style>.rs-calendar-time-dropdown-column ::after {display: none;}</style>`)
@@ -12,27 +17,30 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <DateRangePicker showOneCalendar ranges={[]} format="dd/MM/yyyy, HH:mm" />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div>
+          <div>
+            <a href="https://vite.dev" target="_blank">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
+          <h1>Vite + React</h1>
+          <div className="card">
+            <ModeToggle />
+            <Button variant="outline" onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+            <DateRangePicker showOneCalendar ranges={[]} format="dd/MM/yyyy, HH:mm" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+        </div>
+      </ThemeProvider>
     </>
   )
 }
--- tools/build_frontend.sh
+++ tools/build_frontend.sh
@@ -14,6 +14,9 @@ npm install
 #npm install react-router-dom react-hook-form react-hot-toast axios js-cookie @react-oauth/google react-select rsuite lightweight-charts
 #npm install -D autoprefixer postcss tailwindcss
 #npx tailwindcss init -p
+# Configure shadcnui
+#npx shadcn@latest init
+#npx shadcn@latest add button
 
 echo
 echo "Frontend built."
