d081a8dde446271eb7f0b3e0d8daff1d6484ad82
Pablo Gómez <pgr866@inlumine.ual.es>
2024-12-06 17:14:22 +0100

    Commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -156,7 +156,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
+    'http://localhost:5173', # your domain in production
 ]
 CORS_ALLOW_CREDENTIALS = True
 
