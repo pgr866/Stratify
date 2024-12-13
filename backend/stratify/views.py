a061ad27a9218397c3e71a5ad5a716d4dbca450d
Pablo Gómez <pgr866@inlumine.ual.es>
2024-12-13 22:43:03 +0100

    Commit

--- backend/stratify/views.py
+++ backend/stratify/views.py
@@ -1,5 +1,4 @@
 import requests
-import time
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
@@ -9,6 +8,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+from google.auth.transport.requests import Request
+from google.oauth2 import id_token
+from github import Github
 
 from .models import User
 from .permissions import IsNotAuthenticated, IsOwner, NoBody
@@ -109,15 +111,8 @@ class GoogleLoginView(APIView):
             if not auth_header or not auth_header.startswith('Bearer '):
                 raise Exception("Missing or invalid Authorization header")
             token = auth_header.split(' ')[1]
-            user_response = requests.get(f'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token={token}')
-            if user_response.status_code != 200:
-                raise Exception("Invalid token")
             
-            user_data = user_response.json()
-            if user_data.get('aud') != settings.VITE_GOOGLE_CLIENT_ID:
-                raise Exception("Token is not from the correct application")
-            if float(user_data['exp']) < time.time():
-                raise Exception("Token has expired")
+            user_data = id_token.verify_oauth2_token(token, Request(), settings.VITE_GOOGLE_CLIENT_ID)
             if not user_data.get('email_verified', False):
                 raise Exception("Email not verified")
             google_email = user_data.get("email")
@@ -176,26 +171,12 @@ class GithubLoginView(APIView):
             token_data = token_response.json()
             if token_response.status_code != 200 or "error" in token_data:
                 raise Exception("Invalid code")
-            access_token = token_data.get('access_token')
+            token = token_data.get('access_token')
             
-            validation_response = requests.post(
-                f'https://api.github.com/applications/{settings.VITE_GITHUB_CLIENT_ID}/token',
-                auth=(settings.VITE_GITHUB_CLIENT_ID, settings.GITHUB_CLIENT_SECRET),
-                json={"access_token": access_token}
-            )
-            if validation_response.status_code != 200 or validation_response.json().get('token') != access_token:
-                raise Exception("Invalid token")
-            
-            user_response = requests.get(
-                'https://api.github.com/user',
-                headers={'Authorization': f'Bearer {access_token}'}
-            )
-            if user_response.status_code != 200:
-                raise Exception("Invalid token")
-            user_data = user_response.json()
-            github_id = user_data.get("id")
-            github_username = user_data.get("login")
-            github_email = user_data.get("email") or f"{github_username}@github.local"
+            user_data = Github(token).get_user()
+            github_id = user_data.id
+            github_username = user_data.login
+            github_email = user_data.email or f"{github_username}@github.local"
             user = get_user_model().objects.filter(github_id=github_id).first()
             if not user:
                 if get_user_model().objects.filter(username=github_username).exists():
