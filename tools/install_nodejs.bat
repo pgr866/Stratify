ba4495aa3ae5a2d25c7d27684b705207347184b3
Pablo Gómez <pgr866@inlumine.ual.es>
2024-12-22 18:42:32 +0100

    Commit

--- tools/install_nodejs.bat
+++ tools/install_nodejs.bat
@@ -2,6 +2,10 @@
 
 :: INSTALL NODE.JS
 
+:: Admin request
+net session >nul 2>&1
+if %errorLevel% neq 0 ( powershell Start-Process "%0" -Verb RunAs & exit /b )
+
 :: Change the current working directory to tools directory
 cd /d %~dp0
 
@@ -12,6 +16,7 @@ set /p version=<nodejs_version.txt
 curl -L -o node-v%version%-x64.msi https://nodejs.org/dist/v%version%/node-v%version%-x64.msi
 msiexec /i node-v%version%-x64.msi /quiet /norestart
 del node-v%version%-x64.msi
+setx PATH "$env:PATH;C:\Program Files\nodejs"
 
 :: Check Node.js version
 node -v
