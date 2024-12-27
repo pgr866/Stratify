71d2f690c12fcb94022e62069fabe585664a07ba
Pablo Gómez <pgr866@inlumine.ual.es>
2024-12-27 04:38:13 +0100

    Commit

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -12,11 +12,11 @@ export function Dashboard() {
     };
 
     return (
-        <div>
+        <div className="text-left">
             <div className="fixed top-4 right-4">
                 <ThemeToggle />
             </div>
-            <img src="/logo.svg" alt="Logo" className="logo size-[25rem]" />
+            {/* <img src="/logo.svg" alt="Logo" className="logo size-[25rem]" /> */}
             <Button onClick={handleLogout}>Log out</Button>
             <DateTimeRangePicker></DateTimeRangePicker>
             <CandleChart></CandleChart>
@@ -25,6 +25,51 @@ export function Dashboard() {
             <h3>Taxing Laughter: The Joke Tax Chronicles</h3>
             <h4>Taxing Laughter: The Joke Tax Chronicles</h4>
             <blockquote>Taxing Laughter: The Joke Tax Chronicles</blockquote>
+            <div className="my-6 w-full overflow-y-auto">
+                <table className="w-full">
+                    <thead>
+                        <tr className="m-0 border-t p-0 even:bg-muted">
+                            <th className="table-head">
+                                King's Treasury
+                            </th>
+                            <th className="table-head">
+                                People's happiness
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="m-0 border-t p-0 even:bg-muted">
+                            <td className="table-item">
+                                Empty
+                            </td>
+                            <td className="table-item">
+                                Overflowing
+                            </td>
+                        </tr>
+                        <tr className="m-0 border-t p-0 even:bg-muted">
+                            <td className="table-item">
+                                Modest
+                            </td>
+                            <td className="table-item">
+                                Satisfied
+                            </td>
+                        </tr>
+                        <tr className="m-0 border-t p-0 even:bg-muted">
+                            <td className="table-item">
+                                Full
+                            </td>
+                            <td className="table-item">
+                                Ecstatic
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <ul className="list">
+                <li>1st level of puns: 5 gold coins</li>
+                <li>2nd level of jokes: 10 gold coins</li>
+                <li>3rd level of one-liners : 20 gold coins</li>
+            </ul>
             <code>Taxing Laughter: The Joke Tax Chronicles</code>
             <p className="lead">Taxing Laughter: The Joke Tax Chronicles</p>
             <p className="large">Taxing Laughter: The Joke Tax Chronicles</p>
