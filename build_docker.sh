6b47adb8adb8a568bcacedbd91bed7e8fe1b558c
Pablo Gómez <pgr866@inlumine.ual.es>
2025-01-18 18:29:10 +0100

    Commit

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,6 @@
 # Add Docker's official GPG key:
-sudo apt update
+sudo apt update -y
+sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -8,16 +9,15 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt install -y docker-compose
-docker-compose --version
+sudo apt update -y
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install Docker Compose
+sudo apt install -y docker-compose
 git clone git@github.com:pgr866/TFG.git
-# Run docker-compose
 cd TFG
-docker-compose up -d --build
+sudo docker-compose up -d --build
 read -p "Press any key to continue..."
-docker ps
 # Get SSL certificate
 sudo apt install -y certbot python3-certbot-nginx
 sudo certbot --nginx -d stratify.eastus.cloudapp.azure.com
