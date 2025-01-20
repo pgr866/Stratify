f765cf65265631ae5c4fb5b5b0efa00cce2899cd
Pablo Gómez <pgr866@inlumine.ual.es>
2025-01-20 17:21:14 +0000

    Commit

--- build_docker.sh
+++ build_docker.sh
@@ -13,6 +13,10 @@ echo \
 sudo apt update -y
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y grub-common grub-efi-amd64-bin grub-efi-amd64-signed grub-pc grub-pc-bin grub2-common
+
 source .env
 # Get nginx SSL certificate
 sudo apt install -y nginx certbot python3-certbot-nginx
@@ -20,14 +24,15 @@ sudo certbot --nginx -d $ALLOWED_HOST --email $EMAIL_HOST_USER --agree-tos --no-
 sudo systemctl stop nginx
 
 # Get postgresql SSL certificate
-mkdir -p ./certs
 sudo openssl genrsa -aes256 -passout pass:$SSL_PASSPHRASE -out /etc/ssl/private/postgresdb.key 2048
 sudo openssl rsa -in /etc/ssl/private/postgresdb.key -passin pass:$SSL_PASSPHRASE -out /etc/ssl/private/postgresdb.key
 sudo openssl req -new -key /etc/ssl/private/postgresdb.key -passin pass:$SSL_PASSPHRASE -out /etc/ssl/certs/postgresdb.csr
 sudo openssl x509 -req -in /etc/ssl/certs/postgresdb.csr -signkey /etc/ssl/private/postgresdb.key -passin pass:$SSL_PASSPHRASE -out /etc/ssl/certs/postgresdb.crt -days 365
-rm ./certs/postgresdb.csr
-sudo chmod -R 600 /etc/ssl
-sudo chown -R 999:999 ./certs
+sudo rm -rf /etc/ssl/certs/postgresdb.csr
+sudo chmod 600 /etc/ssl/private/postgresdb.key
+sudo chmod 600 /etc/ssl/certs/postgresdb.crt
+sudo chown 999:999 /etc/ssl/private/postgresdb.key
+sudo chown 999:999 /etc/ssl/certs/postgresdb.crt
 
 # Install Docker Compose
 sudo apt install -y docker-compose
