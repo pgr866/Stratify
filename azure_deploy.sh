2c46ac5a1256f4996109e1fe73ac29228afc53de
Pablo Gómez <pgr866@inlumine.ual.es>
2025-01-22 23:08:19 +0100

    Commit

--- azure_deploy.sh
+++ azure_deploy.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y grub-common grub-efi-amd64-bin grub-efi-amd64-signed grub-pc grub-pc-bin grub2-common
+sudo apt -o APT::Get::Always-Include-Phased-Updates=true upgrade -y
 
 source .env
 # Get nginx SSL certificate
-sudo apt install -y nginx certbot python3-certbot-nginx
-sudo certbot --nginx -d $ALLOWED_HOST --email $EMAIL_HOST_USER --agree-tos --no-eff-email
+if [ ! -f /etc/letsencrypt/live/${ALLOWED_HOST}/fullchain.pem ] || [ ! -f /etc/letsencrypt/live/${ALLOWED_HOST}/privkey.pem ]; then
+    sudo rm -rf /etc/letsencrypt/live/${ALLOWED_HOST}
+    sudo apt install -y nginx certbot python3-certbot-nginx
+    sudo certbot --nginx -d $ALLOWED_HOST --email $EMAIL_HOST_USER --agree-tos --no-eff-email
+fi
 
 # Get postgresql SSL certificate
-sudo openssl genrsa -aes256 -passout pass:$SSL_PASSPHRASE -out /etc/ssl/private/postgresdb.key 2048
-sudo openssl req -new -key /etc/ssl/private/postgresdb.key -passin pass:$SSL_PASSPHRASE -out /etc/ssl/certs/postgresdb.csr
-sudo openssl x509 -req -in /etc/ssl/certs/postgresdb.csr -signkey /etc/ssl/private/postgresdb.key -passin pass:$SSL_PASSPHRASE -out /etc/ssl/certs/postgresdb.crt -days 365
-sudo rm -rf /etc/ssl/certs/postgresdb.csr
-sudo chmod 600 /etc/ssl/private/postgresdb.key
-sudo chmod 600 /etc/ssl/certs/postgresdb.crt
-sudo chown 999:999 /etc/ssl/private/postgresdb.key
-sudo chown 999:999 /etc/ssl/certs/postgresdb.crt
+if [ ! -f /etc/ssl/private/postgresdb.key ] || [ ! -f /etc/ssl/certs/postgresdb.crt ]; then
+    sudo rm -f /etc/ssl/private/postgresdb.key /etc/ssl/certs/postgresdb.crt
+    sudo openssl genrsa -aes256 -passout pass:$SSL_PASSPHRASE -out /etc/ssl/private/postgresdb.key 2048
+    sudo openssl req -new -key /etc/ssl/private/postgresdb.key -passin pass:$SSL_PASSPHRASE -out /etc/ssl/certs/postgresdb.csr
+    sudo openssl x509 -req -in /etc/ssl/certs/postgresdb.csr -signkey /etc/ssl/private/postgresdb.key -passin pass:$SSL_PASSPHRASE -out /etc/ssl/certs/postgresdb.crt -days 365
+    sudo rm -rf /etc/ssl/certs/postgresdb.csr
+    sudo chmod 600 /etc/ssl/private/postgresdb.key
+    sudo chmod 600 /etc/ssl/certs/postgresdb.crt
+    sudo chown 999:999 /etc/ssl/private/postgresdb.key
+    sudo chown 999:999 /etc/ssl/certs/postgresdb.crt
+fi
 
 # Install Docker Compose
 sudo apt install -y docker-compose
