f6029b1382c726d0a1d7be9160ea0abdb07e865b
Pablo Gómez <pgr866@inlumine.ual.es>
2025-01-28 02:10:58 +0100

    Commit

--- azure_deploy.sh
+++ azure_deploy.sh
@@ -30,7 +30,16 @@ if sudo test ! -f /etc/ssl/private/postgresdb.key || sudo test ! -f /etc/ssl/cer
     sudo chown 999:999 /etc/ssl/certs/postgresdb.crt
 fi
 
-# Install Docker Compose
-sudo apt install -y docker-compose
+# Install Docker
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo service docker start
 sudo systemctl stop nginx
-bash -c 'sudo docker-compose down && sudo docker-compose up -d --build'
+bash -c 'sudo docker compose down && sudo docker compose up -d --build'
