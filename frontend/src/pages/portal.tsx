b68f7f5b1a4f2b38eeaa1b1a20e7fe5bb6c63eff
Pablo Gómez <pgr866@inlumine.ual.es>
2025-02-03 20:35:08 +0100

    Commit

--- frontend/src/pages/portal.tsx
+++ frontend/src/pages/portal.tsx
@@ -7,52 +7,54 @@ import { UserNav } from "@/components/user-nav"
 import { Input } from "@/components/ui/input"
 import { Dashboard } from "@/components/dashboard"
 import { Settings } from "@/components/settings"
+import { ThemeToggle } from "@/components/theme-toggle"
 
 export function Portal() {
-  return (
-    <SidebarProvider defaultOpen={false}>
-      <AppSidebar />
-      <SidebarInset style={{ margin: '0' }}>
-        <Tabs defaultValue="dashboard">
-          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-            <div className="flex-col flex">
-              <div className="border-b">
-                <div className="flex h-auto items-center mt-2 xl:mt-0 space-y-2 flex-wrap">
-                  <SidebarTrigger />
-                  <Separator orientation="vertical" className="mr-3 ml-1 h-4" style={{ marginTop: '0' }} />
-                  <TeamSwitcher />
-                  <TabsList className="bg-transparent sm:ml-4" style={{ marginTop: '0' }}>
-                    <TabsTrigger className="px-2 md:px-3" value="dashboard">Dashboard</TabsTrigger>
-                    <TabsTrigger className="px-2 md:px-3" value="customers">Customers</TabsTrigger>
-                    <TabsTrigger className="px-2 md:px-3" value="products">Products</TabsTrigger>
-                    <TabsTrigger className="px-2 md:px-3" value="settings">Settings</TabsTrigger>
-                  </TabsList>
-                  <div className="flex lg:ml-auto items-center space-x-4 pb-2">
-                    <Input
-                      type="search"
-                      placeholder="Search..."
-                      className="w-[150px] lg:w-[250px] xl:w-[300px]"
-                    />
-                    <UserNav />
-                  </div>
-                </div>
-              </div>
-              <TabsContent value="dashboard" className="space-y-4">
-                <Dashboard />
-              </TabsContent>
-              <TabsContent value="customers" className="space-y-4">
+	return (
+		<SidebarProvider defaultOpen={false}>
+			<AppSidebar />
+			<SidebarInset style={{ margin: '0' }}>
+				<Tabs defaultValue="dashboard">
+					<div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+						<div className="flex-col flex">
+							<div className="border-b">
+								<div className="flex h-auto items-center mt-2 xl:mt-0 space-y-2 flex-wrap">
+									<SidebarTrigger />
+									<Separator orientation="vertical" className="mr-3 ml-1 h-4" style={{ marginTop: '0' }} />
+									<TeamSwitcher />
+									<TabsList className="bg-transparent sm:ml-4" style={{ marginTop: '0' }}>
+										<TabsTrigger className="px-2 md:px-3" value="dashboard">Dashboard</TabsTrigger>
+										<TabsTrigger className="px-2 md:px-3" value="customers">Customers</TabsTrigger>
+										<TabsTrigger className="px-2 md:px-3" value="products">Products</TabsTrigger>
+										<TabsTrigger className="px-2 md:px-3" value="settings">Settings</TabsTrigger>
+									</TabsList>
+									<div className="flex lg:ml-auto items-center space-x-4 pb-2">
+										<Input
+											type="search"
+											placeholder="Search..."
+											className="w-[150px] lg:w-[250px] xl:w-[300px]"
+										/>
+										<UserNav />
+										<ThemeToggle />
+									</div>
+								</div>
+							</div>
+							<TabsContent value="dashboard" className="space-y-4">
+								<Dashboard />
+							</TabsContent>
+							<TabsContent value="customers" className="space-y-4">
 
-              </TabsContent>
-              <TabsContent value="products" className="space-y-4">
+							</TabsContent>
+							<TabsContent value="products" className="space-y-4">
 
-              </TabsContent>
-              <TabsContent value="settings" className="space-y-4">
-                <Settings />
-              </TabsContent>
-            </div>
-          </div>
-        </Tabs>
-      </SidebarInset>
-    </SidebarProvider>
-  )
+							</TabsContent>
+							<TabsContent value="settings" className="space-y-4">
+								<Settings />
+							</TabsContent>
+						</div>
+					</div>
+				</Tabs>
+			</SidebarInset>
+		</SidebarProvider>
+	)
 }
