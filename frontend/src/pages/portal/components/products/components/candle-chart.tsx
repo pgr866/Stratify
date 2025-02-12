b526320cd3a33d4cd2b337b0121bc832cec906e1
Pablo Gómez <pgr866@inlumine.ual.es>
2025-02-12 23:03:13 +0100

    Commit

--- frontend/src/pages/portal/components/products/components/candle-chart.tsx
+++ frontend/src/pages/portal/components/products/components/candle-chart.tsx
@@ -1,50 +1,32 @@
 import { useEffect } from "react";
 import { createChart, CandlestickSeries, HistogramSeries, LineSeries, CrosshairMode, createSeriesMarkers } from "lightweight-charts";
 
-let randomFactor = 25 + Math.random() * 25;
-const samplePoint = (i) => i * (0.5 + Math.sin(i / 10) * 0.2 + Math.sin(i / 20) * 0.4 + Math.sin(i / randomFactor) * 0.8 + Math.sin(i / 500) * 0.5) + 200;
-
 function generateLineData(numberOfPoints = 500, endDate) {
-	randomFactor = 25 + Math.random() * 25;
+	const randomFactor = 25 + Math.random() * 25;
 	const res = [];
 	const date = endDate || new Date(Date.UTC(2018, 0, 1, 12, 0, 0, 0));
 	date.setUTCDate(date.getUTCDate() - numberOfPoints - 1);
 	for (let i = 0; i < numberOfPoints; ++i) {
 		const time = date.getTime() / 1000;
-		const value = samplePoint(i);
+		const value = i * (0.5 + Math.sin(i / 10) * 0.2 + Math.sin(i / 20) * 0.4 + Math.sin(i / randomFactor) * 0.8 + Math.sin(i / 500) * 0.5) + 200;
 		res.push({ time, value });
 		date.setUTCDate(date.getUTCDate() + 1);
 	}
 	return res;
 }
 
-function randomNumber(min, max) {
-	return Math.random() * (max - min) + min;
-}
-
-function randomBar(lastClose) {
-	const open = +randomNumber(lastClose * 0.95, lastClose * 1.05).toFixed(2);
-	const close = +randomNumber(open * 0.95, open * 1.05).toFixed(2);
-	const high = +randomNumber(Math.max(open, close), Math.max(open, close) * 1.1).toFixed(2);
-	const low = +randomNumber(Math.min(open, close) * 0.9, Math.min(open, close)).toFixed(2);
-	const volume = Math.floor(randomNumber(1000, 5000) * Math.abs(close - open));
-	return { open, high, low, close, volume };
-}
-
 function generateCandleData(numberOfPoints = 250, endDate) {
+	const randomNumber = (min, max) => Math.random() * (max - min) + min;
 	const lineData = generateLineData(numberOfPoints, endDate);
 	let lastClose = lineData[0].value;
 	return lineData.map((d) => {
-		const candle = randomBar(lastClose);
-		lastClose = candle.close;
-		return {
-			time: d.time,
-			low: candle.low,
-			high: candle.high,
-			open: candle.open,
-			close: candle.close,
-			volume: candle.volume,
-		};
+		const open = +(randomNumber(lastClose * 0.95, lastClose * 1.05)).toFixed(2);
+		const close = +(randomNumber(open * 0.95, open * 1.05)).toFixed(2);
+		const high = +(randomNumber(Math.max(open, close), Math.max(open, close) * 1.1)).toFixed(2);
+		const low = +(randomNumber(Math.min(open, close) * 0.9, Math.min(open, close))).toFixed(2);
+		const volume = Math.floor(randomNumber(1000, 5000) * Math.abs(close - open));
+		lastClose = close;
+		return { time: d.time, open, high, low, close, volume };
 	});
 }
 
@@ -65,9 +47,10 @@ function calculateMovingAverageSeriesData(candleData, maLength) {
 	return maData;
 }
 
-const getCssColor = (name) => `hsl(${getComputedStyle(document.documentElement).getPropertyValue(name).trim()})`;
+let legend;
 
 function getChartOptions() {
+	const getCssColor = (name) => `hsl(${getComputedStyle(document.documentElement).getPropertyValue(name).trim()})`;
 	return {
 		layout: {
 			width: '100%',
@@ -106,100 +89,85 @@ function getChartOptions() {
 	};
 }
 
-export function CandleChart() {
-	useEffect(() => {
-		const chartContainer = document.getElementById('chart-container');
-		if (!chartContainer) return;
+function createCandleChart(candleData, markers) {
+	const chartContainer = document.getElementById('chart-container');
+	if (!chartContainer) return;
+	const chart = createChart(chartContainer, getChartOptions());
+	chart.timeScale().fitContent();
+	chartContainer.style.position = 'relative';
+	const candlestickSeries = chart.addSeries(CandlestickSeries, {
+		upColor: '#2EBD85',
+		downColor: '#F6465D',
+		borderVisible: false,
+		wickUpColor: '#2EBD85',
+		wickDownColor: '#F6465D',
+	});
+	candlestickSeries.setData(candleData);
 
-		const barData = generateCandleData(500);
-		const maData = calculateMovingAverageSeriesData(barData, 20);
+	const volumeData = candleData.map(bar => ({
+		time: bar.time,
+		value: bar.volume,
+		color: bar.close > bar.open ? 'rgba(46, 189, 133, 0.5)' : 'rgba(246, 70, 93, 0.5)'
+	}));
+	const volumeSeries = chart.addSeries(HistogramSeries, {
+		priceFormat: { type: 'volume' },
+		priceScaleId: '',
+	});
+	volumeSeries.priceScale().applyOptions({
+		scaleMargins: {
+			top: 0.8,
+			bottom: 0,
+		},
+	});
+	volumeSeries.setData(volumeData);
 
-		const chart = createChart(chartContainer, getChartOptions());
-		chart.timeScale().fitContent();
-		chartContainer.style.position = 'relative';
-		const candlestickSeries = chart.addSeries(CandlestickSeries, {
-			upColor: '#2EBD85',
-			downColor: '#F6465D',
-			borderVisible: false,
-			wickUpColor: '#2EBD85',
-			wickDownColor: '#F6465D',
+	createSeriesMarkers(candlestickSeries, markers);
+
+	document.documentElement.style.setProperty('--background-alpha', `rgba(${document.documentElement.classList.contains("dark") ? '26,26,26' : '229,229,229'},0.7)`);
+	legend = Object.assign(document.createElement('div'), {
+		style: 'position:absolute;left:8px;top:7px;z-index:10;font-size:13px;font-weight:400;',
+	});
+	chartContainer.appendChild(legend);
+	chart.subscribeCrosshairMove((param) => {
+		const candleData = param.seriesData.get(candlestickSeries)
+			?? candlestickSeries.data().at(-1)
+			?? '—';
+		const volumeData = param.seriesData.get(volumeSeries)
+			?? volumeSeries.data().at(-1)
+			?? '—';
+		const { open: o, high: h, low: l, close: c } = candleData;
+		const change = ((c - o) / o * 100).toFixed(2);
+		const formattedVolume = volumeData.value >= 1000000
+			? (volumeData.value / 1000000).toFixed(2) + ' M'
+			: volumeData.value >= 1000
+				? (volumeData.value / 1000).toFixed(2) + ' K'
+				: volumeData.value;
+		legend.innerHTML = `
+			<text>
+			O <span>${o}</span>
+			H <span>${h}</span> 
+			L <span>${l}</span> 
+			C <span>${c} (${change > 0 ? '+' : ''}${change}%)</span> 
+			V <span>${formattedVolume}</span>
+			`;
+		legend.querySelectorAll('span').forEach(el => el.style.color = c >= o ? '#2EBD85' : '#F6465D');
+		legend.querySelectorAll('text').forEach(el => {
+			el.style.backgroundColor = 'var(--background-alpha)';
+			el.style.padding = '0 5px';
+			el.style.borderRadius = '5px';
 		});
-		candlestickSeries.setData(barData);
+	});
+	return chart;
+}
 
-		const volumeData = barData.map(bar => ({
-			time: bar.time,
-			value: bar.volume,
-			color: bar.close > bar.open ? 'rgba(46, 189, 133, 0.5)' : 'rgba(246, 70, 93, 0.5)'
-		}));
-		const volumeSeries = chart.addSeries(HistogramSeries, {
-			priceFormat: { type: 'volume' },
-			priceScaleId: '',
+function addLine(chart, lineData, pane, color) {
+	const chartContainer = document.getElementById('chart-container');
+	const lineSeries = chart.addSeries(LineSeries, { color: color, lineWidth: 1 }, pane);
+	lineSeries.setData(lineData);
+	if (pane > 0) {
+		const newLegend = Object.assign(document.createElement('div'), {
+			style: 'position:absolute;left:8px;top:7px;z-index:10;font-size:13px;font-weight:400;',
 		});
-		volumeSeries.priceScale().applyOptions({
-			scaleMargins: {
-				top: 0.8,
-				bottom: 0,
-			},
-		});
-		volumeSeries.setData(volumeData);
-
-		const maSeries = chart.addSeries(LineSeries, { color: 'blue', lineWidth: 1 });
-		maSeries.setData(maData);
-
-		const markers = [
-			{
-				time: barData[barData.length - 2].time,
-				position: 'aboveBar',
-				color: '#F6465D',
-				shape: 'arrowDown',
-				text: 'Sell @1',
-			},
-			{
-				time: barData[barData.length - 1].time,
-				position: 'belowBar',
-				color: '#2EBD85',
-				shape: 'arrowUp',
-				text: 'Buy @2',
-			}
-		];
-		createSeriesMarkers(candlestickSeries, markers);
-
-		document.documentElement.style.setProperty('--background-alpha', `rgba(${document.documentElement.classList.contains("dark") ? '26,26,26' : '229,229,229'},0.7)`);
-		const chartLegend = Object.assign(document.createElement('div'), {
-			style: 'position:absolute;left:15px;top:7px;z-index:10;font-size:13px;font-weight:400;background:var(--background-alpha);padding: 0 5px;border-radius:5px;',
-		});
-		chartContainer.appendChild(chartLegend);
-		const updateChartLegend = (param) => {
-			const candleData = param.seriesData.get(candlestickSeries)
-				?? candlestickSeries.dataByIndex(candlestickSeries.data().length - 1)
-				?? '—';
-			const volumeData = param.seriesData.get(volumeSeries)
-				?? volumeSeries.dataByIndex(volumeSeries.data().length - 1)
-				?? '—';
-			const { open: o, high: h, low: l, close: c } = candleData;
-			const change = ((c - o) / o * 100).toFixed(2);
-			const formattedVolume = volumeData.value >= 1000000
-				? (volumeData.value / 1000000).toFixed(2) + ' M'
-				: volumeData.value >= 1000
-					? (volumeData.value / 1000).toFixed(2) + ' K'
-					: volumeData.value;
-			chartLegend.innerHTML = `
-						O <span>${o}</span> 
-						H <span>${h}</span> 
-						L <span>${l}</span> 
-						C <span>${c} (${change > 0 ? '+' : ''}${change}%)</span> 
-						V <span>${formattedVolume}</span>
-				`;
-			chartLegend.querySelectorAll('span').forEach(el => el.style.color = c >= o ? '#2EBD85' : '#F6465D');
-		}
-		chart.subscribeCrosshairMove(updateChartLegend);
-
-		const subChartSeries = chart.addSeries(LineSeries, { color: 'blue', lineWidth: 1 }, 1);
-		subChartSeries.setData(generateLineData(500));
-		const subChartLegend = Object.assign(document.createElement('div'), {
-			style: 'position:absolute;left:15px;top:7px;z-index:10;font-size:13px;font-weight:400;background:var(--background-alpha);padding: 0 5px;border-radius:5px;',
-		});
-
 		requestAnimationFrame(function checkTable() {
 			const table = chartContainer.querySelector('table');
 			if (!table || table.children.length < 4) {
@@ -207,38 +175,87 @@ export function CandleChart() {
 				return;
 			}
 			const divsWithCanvas = Array.from(table.children);
-			divsWithCanvas.at(-2).style.position = 'relative';
-			divsWithCanvas.at(-2).appendChild(subChartLegend);
+			divsWithCanvas.at(2 * pane).style.position = 'relative';
+			divsWithCanvas.at(2 * pane).appendChild(newLegend);
 		});
-		const updateSubChartLegend = (param) => {
-			const subChartData = param.seriesData.get(subChartSeries)
-				?? subChartSeries.dataByIndex(subChartSeries.data().length - 1)
+		chart.subscribeCrosshairMove((param) => {
+			const lineData = param.seriesData.get(lineSeries)
+				?? lineSeries.data().at(-1)
 				?? '—';
-			subChartLegend.innerHTML = `SubChart <span>${subChartData.value.toFixed(2)}</span>`;
-			subChartLegend.querySelector('span').style.color = 'blue';
-		}
-		chart.subscribeCrosshairMove(updateSubChartLegend);
-
-		const resizeChart = () => {
-			let { width, height } = chartContainer.getBoundingClientRect();
-			chart.resize(width, height);
-			chartContainer.querySelector('.tv-lightweight-charts').style.width = '100%';
-			chartContainer.querySelector('.tv-lightweight-charts').style.height = '100%';
-			chartContainer.querySelector('table').style.width = '100%';
-			chartContainer.querySelector('table').style.height = '100%';
-		};
-		const resizeObserver = new ResizeObserver(resizeChart);
-		resizeObserver.observe(chartContainer);
-		const themeObserver = new MutationObserver(() => {
-			document.documentElement.style.setProperty('--background-alpha', `rgba(${document.documentElement.classList.contains("dark") ? '26,26,26' : '229,229,229'},0.7)`);
-			chart.applyOptions(getChartOptions());
+			newLegend.innerHTML = `<text>Line <span style="color:${color}">${lineData.value.toFixed(2)}</span>`;
+			newLegend.querySelectorAll('text').forEach(el => {
+				el.style.backgroundColor = 'var(--background-alpha)';
+				el.style.padding = '0 5px';
+				el.style.borderRadius = '5px';
+			});
 		});
-		themeObserver.observe(document.documentElement, { attributes: true, attributeFilter: ["style", "class"] });
-		return () => {
-			resizeObserver.disconnect();
-			themeObserver.disconnect();
-			chart.remove();
-		};
+	} else {
+		chart.subscribeCrosshairMove((param) => {
+			const lineData = param.seriesData.get(lineSeries)
+				?? lineSeries.data().at(-1)
+				?? '—';
+			legend.innerHTML += `<br><text>Line <span style="color:${color}">${lineData.value.toFixed(2)}</span>`;
+			legend.querySelectorAll('text').forEach(el => {
+				el.style.backgroundColor = 'var(--background-alpha)';
+				el.style.padding = '0 5px';
+				el.style.borderRadius = '5px';
+			});
+		});
+	}
+}
+
+function updateChart(chart) {
+	const chartContainer = document.getElementById('chart-container');
+	const resizeChart = () => {
+		let { width, height } = chartContainer.getBoundingClientRect();
+		chart.resize(width, height);
+		chartContainer.querySelector('.tv-lightweight-charts').style.width = '100%';
+		chartContainer.querySelector('.tv-lightweight-charts').style.height = '100%';
+		chartContainer.querySelector('table').style.width = '100%';
+		chartContainer.querySelector('table').style.height = '100%';
+	};
+	const resizeObserver = new ResizeObserver(resizeChart);
+	resizeObserver.observe(chartContainer);
+	const themeObserver = new MutationObserver(() => {
+		document.documentElement.style.setProperty('--background-alpha', `rgba(${document.documentElement.classList.contains("dark") ? '26,26,26' : '229,229,229'},0.7)`);
+		chart.applyOptions(getChartOptions());
+	});
+	themeObserver.observe(document.documentElement, { attributes: true, attributeFilter: ["style", "class"] });
+	return () => {
+		resizeObserver.disconnect();
+		themeObserver.disconnect();
+		chart.remove();
+	};
+}
+
+export function CandleChart() {
+	const candleData = generateCandleData(500);
+	const markers = [
+		{
+			time: candleData[candleData.length - 2].time,
+			position: 'aboveBar',
+			color: '#F6465D',
+			shape: 'arrowDown',
+			text: '@1 -15.34',
+		},
+		{
+			time: candleData[candleData.length - 1].time,
+			position: 'belowBar',
+			color: '#2EBD85',
+			shape: 'arrowUp',
+			text: '@2 +15.34',
+		}
+	];
+	const maData = calculateMovingAverageSeriesData(candleData, 20);
+	const subChartData = generateLineData(500);
+
+	useEffect(() => {
+		const chart = createCandleChart(candleData, markers);
+		addLine(chart, maData, 0, 'blue');
+		addLine(chart, maData, 0, 'red');
+		addLine(chart, subChartData, 1, 'purple');
+		//addLine(chart, subChartData, 2, 'orange');
+		return updateChart(chart);
 	}, []);
 
 	return (
