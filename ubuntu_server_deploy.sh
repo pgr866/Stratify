bdf25d8c5029e65895665beff24337af6964cbae
Pablo Gómez <pgr866@inlumine.ual.es>
2025-05-14 20:53:11 +0000

    Commit

--- ubuntu_server_deploy.sh
+++ ubuntu_server_deploy.sh
@@ -85,6 +85,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo apt autoremove -y
 
